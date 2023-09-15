d91ce81be2094c6600d4c6d123b2e24399fd278b
Anel6969 <115752275+Anel6969@users.noreply.github.com>
2023-09-15 15:19:38 -0400

    plusieurs ajustement avant de remettre

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,36 @@
 const CACHE_NAME = 'static-cache-v1';
 const FILES_TO_CACHE = [
-  'offline.html'
+  'offline.html',
+  'index.html',
+  'zoneclient.html',
+  'aproposnous.html',
+  'confrmation.html',
+  'css/styles.css',
+  'script/install.js',
+  'script/script.js',
+  'manifest.json',
+  'style/tailwind.config.js',
+  'images/icons/icon-144x144.png',
+  'images/icons/icon-192x192.png',
+  'images/icons/icon-512x512.png',
+  'images/batisses-2.jpg',
+  'images/batisses.jpg',
+  'images/bottes_de_travail.jpg',
+  'images/BotteTimberlandPro.png',
+  'images/BotteTimberlandPro6pouce.png',
+  'images/CentreTS_Slogan_CMYK.jpg',
+  'images/image-extraits-defi.jpg',
+  'images/logo.png',
+  'images/shutterstock_397846903.jpg',
+  'images/shutterstock_1647660442.jpg',
+  'images/shutterstock_1732967150.jpg',
+  'images/Soulier_de_travail.jpg',
+  'images/store.jpg',
+  'images/vesteColumbia.png',
+  'images/vet_de_travail.jpg',
+  'images/vet_perso.jpg',
+  'images/vetCat.png',
 ];
-
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
@@ -17,20 +45,15 @@ self.addEventListener('install', (evt) => {
   self.skipWaiting();
 });
 
-
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'installPWA') {
- 
     event.source.postMessage({ type: 'installPWAConfirmed' });
   }
 });
 
-
-
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
-  
-  
+
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
@@ -41,16 +64,13 @@ self.addEventListener('activate', (evt) => {
       }));
     })
   );
-  
-  
+
   self.clients.claim();
 });
 
-
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
-  
-  
+
   if (evt.request.mode === 'navigate') {
     evt.respondWith(
       fetch(evt.request).catch(() => {
